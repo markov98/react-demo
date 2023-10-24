b06c7eea496f38eb9f98274a8bafec8b319e28fd
markov98 <74935630+markov98@users.noreply.github.com>
2023-10-24 19:29:42 +0300

    added images from template

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,14 +4,14 @@ function Header() {
             <header className="header_section">
                 <div className="container-fluid">
                     <nav className="navbar navbar-expand-lg custom_nav-container">
-                        <a className="navbar-brand" href="#">
+                        <a className="navbar-brand" href="index.html">
                             <img src="images/logo.png" alt="" />
                             <span>Spering</span>
                         </a>
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
                             <ul className="navbar-nav">
                                 <li className="nav-item active">
                                     <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
@@ -33,7 +33,6 @@ function Header() {
                                 <form className="form-inline my-2 my-lg-0 ml-0 ml-lg-4 mb-3 mb-lg-0">
                                     <button className="btn my-2 my-sm-0 nav_search-btn" type="submit"></button>
                                 </form>
-                            </div>
                         </div>
                         <div className="custom_menu-btn">
                             <button>
