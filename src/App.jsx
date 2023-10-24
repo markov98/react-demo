5a14245a2f7e35d7dd5849ee3fc2ba07bc00b7e9
markov98 <74935630+markov98@users.noreply.github.com>
2023-10-25 02:14:56 +0300

    added slider component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import Header from './Header';
-
+import Header from "./components/Header"
+import Slider from "./components/Slider"
 function App() {
 
   return (
     <>
       <div className="hero_area">
         <Header />
+        <Slider />
       </div>
     </>
   )
 }
 
-export default App
+export default App;
